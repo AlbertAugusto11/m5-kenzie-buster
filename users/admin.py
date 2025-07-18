c16a5a543d1073a1a70d33f6b18bb04b55cdf120
Albert Augusto <167814598+AlbertAugusto11@users.noreply.github.com>
2025-07-18 15:34:25 -0300

    Adicionando Interface UserAdmin

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,25 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    readonly_fields = ("id", "is_superuser")
+
+    fieldsets = (
+        ("Credenciais", {"fields": ("username", "password")}),
+        (
+            "Personal Info",
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        ("Permissions", {"fields": ("is_employee", "is_superuser")}),
+        ("Dates", {"fields": ("birthdate",)}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
